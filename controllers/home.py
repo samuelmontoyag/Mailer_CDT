3a6e41c0166f838bb9e67e2689fab7587d3664f2
Francisco Vergara <javiervergara2004@gmail.com>
2017-06-15 21:19:09 -0400

    FIX: Se elimina la consulta de eventos de controlador de campa?as.

---
+++ controllers/home.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from flask import (
+    Blueprint,
+    current_app,
+    Response,
+    abort,
+    jsonify,
+    request,
+    session,
+    redirect,
+    flash,
+    render_template,
+    url_for
+)
+from flask_login import login_required, current_user
+from sqlalchemy import Table
+from utils import get_mailchimp_api
+import json
+home = Blueprint('home', __name__)
+
+
+def initialize_app(app):
+    app.register_blueprint(home)
+
+
+@home.route("/", methods=['GET', 'POST'])
+@login_required
+def index():
+    return render_template('index.html')
+
+
+@home.route("/statistics", methods=['GET', 'POST'])
+@login_required
+def statistics():
+    m = get_mailchimp_api()
+    data = {'mailing_count': m.campaigns.list(limit=0).get('total', 0),
+            'template_count': len(m.templates.list().get('user')),
+            'emails_list_count': m.lists.list(limit=0).get('total', 0)
+            }
+    return json.dumps(data)
