781c3b91a8b7af9507d28f02bcba7817bab96dee
smontoya <samuelmontoyag@gmail.com>
2017-06-27 07:01:01 -0400

    ADD: espacio de disco

--- controllers/home.py
+++ controllers/home.py
@@ -16,6 +16,7 @@ from flask_login import login_required, current_user
 from sqlalchemy import Table
 from utils import get_mailchimp_api
 import json
+import os
 home = Blueprint('home', __name__)
 
 
@@ -37,4 +38,13 @@ def statistics():
             'template_count': len(m.templates.list().get('user')),
             'emails_list_count': m.lists.list(limit=0).get('total', 0)
             }
+    return json.dumps(data)
+
+
+@home.route("/disk_usage", methods=['GET', 'POST'])
+@login_required
+def disk_usage():
+    st = os.statvfs('/')
+    data = {'free_space': (st.f_bavail * st.f_frsize) / 1024,
+            'used_disk': ((st.f_blocks - st.f_bfree) * st.f_frsize) / 1024}
     return json.dumps(data)
